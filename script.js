c3e09b27a70b5fe0fe1b16881b92cbc57552fac5
filipasegor <filipasegor@gmail.com>
2020-11-02 01:44:34 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let newSchool = [
+  [
+    ['Lil Yachty', 'Playboi Carti', 'Lil Uzi Vert'],
+    ['Sonny Digital', 'Metro Boomin', '12Hunna']
+  ],
+  [
+    ['Yung Lean', 'Ecco2K', 'Bladee'],
+    ['Whitearmor', 'PJ Beats', 'Yung Gud']
+  ]
+]
+
+
+newSchool.forEach((item, i, newSchool ) => {
+  console.log( i + item );
+});
