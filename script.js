e1e1f6a7bfd7148755372f0613d65a9150c5606e
filipasegor <filipasegor@gmail.com>
2020-11-27 16:42:24 +0300

    first push

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let newSchool = [
-  [
-    ['Lil Yachty', 'Playboi Carti', 'Lil Uzi Vert'],
-    ['Sonny Digital', 'Metro Boomin', '12Hunna']
-  ],
-  [
-    ['Yung Lean', 'Ecco2K', 'Bladee'],
-    ['Whitearmor', 'PJ Beats', 'Yung Gud']
-  ]
+let crypto = [
+  {
+    name: "Bitcoin",
+    price: 1388.37
+  },
+  {
+    name: "Ethereum",
+    price: 716
+  },
+  {
+    name: "Litecoin",
+    price: 140
+  }
 ]
 
 
-for(let j = 0; j < newSchool.length; j++) {
-  for(let n = 0; n < newSchool.length; n++) {
-    console.log(newSchool[j][n]);
-  }
+
+
+const forms = document.querySelectorAll('form')
+
+const progressBarInner = document.getElementsByClassName('progress-bar-inner');
+console.log(progressBarInner[1]);
+
+const par = document.querySelectorAll('p');
+console.log(par);
+
+for (form of forms){
+  console.log(form);
+};
+
+// document.getElementById("bitcoin-inner").innerHTML = crypto[0].price;
+// document.getElementById("ethereum-inner").innerHTML = crypto[1].price;
+// document.getElementById("litecoin-inner").innerHTML = crypto[2].price;
+
+
+for (let i = 0; i < crypto.length; i++){
+  par[i].innerHTML = crypto[i].price;
+  progressBarInner[i].style.width = ((crypto[i].price * 100) / 1388.37) + '%';
 }
